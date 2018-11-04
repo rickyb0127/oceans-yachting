f690fa5b6a25e49965fe40ac4acdb35327103f09
eric bartsch <bartsched@gmail.com>
2018-11-04 10:03:50 -0700

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :find_screen_size
+
+  def find_screen_size
+    @regularScreen = false
+  end
 end
