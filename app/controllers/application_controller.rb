4a60a7545c619d922a36572fbedb68d604af4a2a
eric bartsch <bartsched@gmail.com>
2018-11-09 08:42:13 -0700

    refactor to one profile-carousel and one profile-modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :find_screen_size
+  before_filter :initialize_session
 
-  def find_screen_size
-    @regularScreen = false
+  def initialize_session
+    @session = session
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  post '/get_screen_size', to: 'welcome#get_screen_size'
 end
